7620ccdf4d3bbe288c71335f9b37c80b24f60798
java-tester-x <iohonko@gmail.com>
2014-11-26 07:28:40 +0200

    Ковариантность возвращаемых типов в Java

---
+++ polymorphism/Transmogrify.java
@@ -0,0 +1,44 @@
+package polymorphism;
+
+class Actor {
+
+    public void act() {}
+}
+
+class HappyActor extends Actor {
+
+    public void act() {
+        System.out.println("HappyActor");
+    }
+}
+
+class SadActor extends Actor {
+
+    public void act() {
+        System.out.println("SadActor");
+    }
+}
+
+class Stage {
+
+    private Actor actor = new HappyActor();
+
+    public void change() {
+        actor = new SadActor();
+    }
+
+    public void performPlay() {
+        actor.act();
+    }
+}
+
+
+public class Transmogrify {
+
+    public static void main(String[] args) {
+        Stage stage = new Stage();
+        stage.performPlay();
+        stage.change();
+        stage.performPlay();
+    }
+}
