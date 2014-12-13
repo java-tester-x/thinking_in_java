9aea4c11a0e8a7baffcebc81a608aee456b7950f
java-tester-x <iohonko@gmail.com>
2014-12-13 22:36:48 +0200

    Inner Classes: simple example

---
+++ innerclasses/Parcel1.java
@@ -0,0 +1,44 @@
+package innerclasses;
+
+/**
+ * RUN:
+ *         javac innerclasses/Parcel1.java && java innerclasses.Parcel1
+ * OUTPUT:
+ *         Tasmania
+ */
+
+public class Parcel1 {
+
+    class Contents {
+        
+        private int i = 11;
+        
+        public int value() {
+            return i;
+        }
+    }
+
+    class Destination {
+
+        private String label;
+
+        Destination(String whereTo) {
+            label = whereTo;
+        }
+
+        String readLabel() {
+            return label;
+        }
+    }
+
+    public void ship(String dest) {
+        Contents c  = new Contents();
+        Destination d = new Destination(dest);
+        System.out.println(d.readLabel());
+    }
+
+    public static void main(String[] args) {
+        Parcel1 p = new Parcel1();
+        p.ship("Tasmania");
+    }
+}
