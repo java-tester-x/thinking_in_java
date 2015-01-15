8e178fa4d9858a7f1000bba086b8644891028f6a
java-tester-x <iohonko@gmail.com>
2015-01-15 23:34:39 +0200

    Exceptions: exception rethrowing example

--- exceptions/Rethrowing.java
+++ exceptions/Rethrowing.java
@@ -4,7 +4,27 @@ package exceptions;
  * RUN:
  *         javac exceptions/Rethrowing.java && java exceptions.Rethrowing
  * OUTPUT:
- *
+ *         Creating exception in f()
+ *         In method g(): e.printStackTrace()
+ *         java.lang.Exception: thrown from f()
+ *                 at exceptions.Rethrowing.f(Rethrowing.java:38)
+ *                 at exceptions.Rethrowing.g(Rethrowing.java:43)
+ *                 at exceptions.Rethrowing.main(Rethrowing.java:66)
+ *         main: printStackTrace()
+ *         java.lang.Exception: thrown from f()
+ *                 at exceptions.Rethrowing.f(Rethrowing.java:38)
+ *                 at exceptions.Rethrowing.g(Rethrowing.java:43)
+ *                 at exceptions.Rethrowing.main(Rethrowing.java:66)
+ *         Creating exception in f()
+ *         In method h(): e.printStackTrace()
+ *         java.lang.Exception: thrown from f()
+ *                 at exceptions.Rethrowing.f(Rethrowing.java:38)
+ *                 at exceptions.Rethrowing.h(Rethrowing.java:54)
+ *                 at exceptions.Rethrowing.main(Rethrowing.java:74)
+ *         main: printStackTrace()
+ *         java.lang.Exception: thrown from f()
+ *                 at exceptions.Rethrowing.h(Rethrowing.java:59)
+ *                 at exceptions.Rethrowing.main(Rethrowing.java:74)
  */
 
 // import java.util.logging.*;
@@ -14,10 +34,48 @@ package exceptions;
 public class Rethrowing {
 
     public static void f() throws Exception {
-
+        System.out.println("Creating exception in f()");
+        throw new Exception("thrown from f()");
     }
 
-    public static void main(String[] args) {
-        
+    public static void g() throws Exception {
+        try {
+            f();
+        }
+        catch(Exception e) {
+            System.out.println("In method g(): e.printStackTrace()");
+            e.printStackTrace(System.out);
+            throw e;
+        }
+    }
+
+    public static void h() throws Exception {
+        try {
+            f();
+        }
+        catch(Exception e) {
+            System.out.println("In method h(): e.printStackTrace()");
+            e.printStackTrace(System.out);
+            throw (Exception)e.fillInStackTrace();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        try {
+            g();
+        }
+        catch(Exception e) {
+            System.out.println("main: printStackTrace()");
+            e.printStackTrace(System.out);
+        }
+
+        try {
+            h();
+        }
+        catch(Exception e) {
+            System.out.println("main: printStackTrace()");
+            e.printStackTrace(System.out);
+        }
     }
 }
