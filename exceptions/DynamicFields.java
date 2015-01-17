7feee3a1ec8c7c318d21089a0be548a36186dc11
java-tester-x <iohonko@gmail.com>
2015-01-17 09:55:44 +0200

    Exceptions: exception initCause() examples

--- exceptions/DynamicFields.java
+++ exceptions/DynamicFields.java
@@ -4,7 +4,26 @@ package exceptions;
  * RUN:
  *         javac exceptions/DynamicFields.java && java exceptions.DynamicFields
  * OUTPUT:
- *          
+ *         null: null
+ *         null: null
+ *         null: null
+ *         
+ *         d: Value of d
+ *         number: 47
+ *         number2: 48
+ *         
+ *         df: d: New value of d
+ *         number: 47
+ *         number2: 48
+ *         number3: 11
+ *         
+ *         df.getField("d")  New value of d
+ *         exceptions.DynamicFieldsException
+ *                 at exceptions.DynamicFields.setField(DynamicFields.java:101)
+ *                 at exceptions.DynamicFields.main(DynamicFields.java:141)
+ *         Caused by: java.lang.NullPointerException
+ *                 at exceptions.DynamicFields.setField(DynamicFields.java:102)
+ *                 ... 1 more
  */
 
 // import java.util.logging.*;
@@ -24,7 +43,7 @@ public class DynamicFields {
 
     public String toString() {
         StringBuilder result = new StringBuilder();
-        for (Object obj : fields) {
+        for (Object[] obj : fields) {
             result.append(obj[0]);
             result.append(": ");
             result.append(obj[1]);
@@ -45,12 +64,13 @@ public class DynamicFields {
     private int getFieldNumber(String id) throws NoSuchFieldException {
         int fieldNum = hasField(id);
         if (fieldNum == -1) {
-            throw new NoSuchFieldException()''
+            throw new NoSuchFieldException();
         }
         return fieldNum;
     }
 
-    private int makeField(String id) {
+    private int makeField(String id)
+    {
         for (int i = 0; i < fields.length; i++) {
             if (fields[i][0] == null) {
                 fields[i][0] = id;
@@ -72,13 +92,15 @@ public class DynamicFields {
     }
 
     public Object getField(String id) throws NoSuchFieldException {
-        return fields[getFieldNumber(i)][1];
+        return fields[getFieldNumber(id)][1];
     }
 
-    public Object setField(String id, Object value) throws DynamicFieldsException {
+    public Object setField(String id, Object value) throws DynamicFieldsException
+    {
         if (value == null) {
             DynamicFieldsException dfe = new DynamicFieldsException();
             dfe.initCause(new NullPointerException());
+            throw dfe;
         }
         
         int fieldNumber = hasField(id);
@@ -99,8 +121,31 @@ public class DynamicFields {
     }
 
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)
+    {
+        DynamicFields df = new DynamicFields(3);
+        System.out.println(df);
+
+        try {
+            df.setField("d", "Value of d");
+            df.setField("number", 47);
+            df.setField("number2", 48);
+            System.out.println(df);
+
+            df.setField("d", "New value of d");
+            df.setField("number3", 11);
+            System.out.println("df: " + df);
+
+            System.out.println("df.getField(\"d\")  " + df.getField("d"));
+
+            Object field = df.setField("d", null);
+        }
+        catch(NoSuchFieldException e) {
+            e.printStackTrace(System.out);
+        }
+        catch(DynamicFieldsException e) {
+            e.printStackTrace(System.out);
+        }
     }
 }
 
