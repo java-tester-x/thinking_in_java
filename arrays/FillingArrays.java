1f88311f432d3041f792ee01c3651ae28c357365
java-tester-x <iohonko@gmail.com>
2015-02-26 07:33:55 +0200

    Arrays: array options example

---
+++ arrays/FillingArrays.java
@@ -0,0 +1,66 @@
+package arrays;
+
+import java.util.*;
+
+/**
+ * RUN:
+ *         javac arrays/FillingArrays.java && java arrays.FillingArrays
+ * OUTPUT:
+ *         a1 = [true, true, true, true, true, true]
+ *         a2 = [11, 11, 11, 11, 11, 11]
+ *         a3 = [x, x, x, x, x, x]
+ *         a4 = [17, 17, 17, 17, 17, 17]
+ *         a5 = [19, 19, 19, 19, 19, 19]
+ *         a6 = [26, 26, 26, 26, 26, 26]
+ *         a7 = [29.0, 29.0, 29.0, 29.0, 29.0, 29.0]
+ *         a8 = [47.0, 47.0, 47.0, 47.0, 47.0, 47.0]
+ *         a9 = [Hello, Hello, Hello, Hello, Hello, Hello]
+ *         a9 = [Hello, Hello, Hello, World, World, Hello]
+ */
+
+public class FillingArrays {
+
+    public static void main(String[] args) {
+        int size = 6;
+
+        boolean[] a1 = new boolean[size];
+        byte[]    a2 = new byte[size];
+        char[]    a3 = new char[size];
+        short[]   a4 = new short[size];
+        int[]     a5 = new int[size];
+        long[]    a6 = new long[size];
+        float[]   a7 = new float[size];
+        double[]  a8 = new double[size];
+        String[]  a9 = new String[size];
+
+        Arrays.fill(a1, true);
+        System.out.println("a1 = " + Arrays.toString(a1));
+
+        Arrays.fill(a2, (byte)11);
+        System.out.println("a2 = " + Arrays.toString(a2));
+
+        Arrays.fill(a3, 'x');
+        System.out.println("a3 = " + Arrays.toString(a3));
+        
+        Arrays.fill(a4, (short)17);
+        System.out.println("a4 = " + Arrays.toString(a4));
+
+        Arrays.fill(a5, 19);
+        System.out.println("a5 = " + Arrays.toString(a5));
+
+        Arrays.fill(a6, 26);
+        System.out.println("a6 = " + Arrays.toString(a6));
+
+        Arrays.fill(a7, 29);
+        System.out.println("a7 = " + Arrays.toString(a7));
+
+        Arrays.fill(a8, 47);
+        System.out.println("a8 = " + Arrays.toString(a8));
+
+        Arrays.fill(a9, "Hello");
+        System.out.println("a9 = " + Arrays.toString(a9));
+
+        Arrays.fill(a9, 3, 5, "World");
+        System.out.println("a9 = " + Arrays.toString(a9));        
+    }
+}
---
+++ arrays/ParametrizedArrayType.java
@@ -0,0 +1,33 @@
+package arrays;
+
+import java.util.*;
+
+/**
+ * RUN:
+ *         javac arrays/ParametrizedArrayType.java && java arrays.ParametrizedArrayType
+ * OUTPUT:
+ *         
+ */
+
+public class ParametrizedArrayType {
+
+    public static void main(String[] args) {
+        Integer[] ints = {1,2,3,4,5};
+        Double[] doubles = {1.1, 2.2, 3.3, 4.4, 5.5};
+
+        Integer[] ints2 = new ClassParameter<Integer>().f(ints);
+        Double[] doubles2 = new ClassParameter<Double>().f(doubles);
+
+
+        ints2 = MethodParameter.f(ints);
+        doubles2 = MethodParameter.f(doubles);
+    }
+}
+
+class ClassParameter<T> {
+    public T[] f(T[] arg) { return arg; }
+}
+
+class MethodParameter {
+    public static <T> T[] f(T[] arg) { return arg; }
+}
