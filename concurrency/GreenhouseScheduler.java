8ceef9600b4e54a773e64cce53d0417c11e863d0
java-tester-x <iohonko@gmail.com>
2015-04-17 07:13:39 +0300

    Concurrency: using scheduleAtFixedRate example

--- concurrency/GreenhouseScheduler.java
+++ concurrency/GreenhouseScheduler.java
@@ -14,7 +14,7 @@ import java.util.*;
 
 public class GreenhouseScheduler {
 
-    private volatile boolean ligth = false;
+    private volatile boolean light = false;
 
     private volatile boolean water = false;
 
@@ -39,34 +39,160 @@ public class GreenhouseScheduler {
     }
 
     class LightOn implements Runnable {
-
+        
+        public void run() {
+            // 
+            // here must be code for microcontroller
+            // 
+            System.out.println("Light On");
+            light = true;
+        }
     }
 
     class LightOff implements Runnable {
         
+        public void run() {
+            // 
+            // here must be code for microcontroller
+            // 
+            System.out.println("Light Off");
+            light = false;
+        }
     }
 
     class WaterOn implements Runnable {
 
+        public void run() {
+            // 
+            // here must be code for microcontroller
+            // 
+            System.out.println("Water On");
+            water = true;
+        }
     }
 
     class WaterOff implements Runnable {
         
+        public void run() {
+            // 
+            // here must be code for microcontroller
+            // 
+            System.out.println("Water Off");
+            water = false;
+        }
     }
 
     class ThermostatNight implements Runnable {
 
+        public void run() {
+            // 
+            // here must be code for microcontroller
+            // 
+            System.out.println("Thermostat set in \"Night\" mode");
+            setThermostat("Night");
+        }
     }
 
     class ThermostatDay implements Runnable {
         
+        public void run() {
+            // 
+            // here must be code for microcontroller
+            // 
+            System.out.println("Thermostat set in \"Day\" mode");
+            setThermostat("Day");
+        }        
     }
 
     class Bell implements Runnable {
         
+        public void run() {
+            System.out.println("B-A-A-M!");
+        }
     }
 
     class Terminate implements Runnable {
         
+        public void run() {
+            System.out.println("Terminating");
+
+            scheduler.shutdownNow();
+
+            new Thread() {
+                public void run() {
+                    for (DataPoint d : data) {
+                        System.out.println(d);
+                    }
+                }
+            }.start();
+        }
+    }
+
+    static class DataPoint {
+        final Calendar time;
+        final float temperature;
+        final float humidity;
+
+        public DataPoint(Calendar d, float temperature, float humidity) {
+            this.time = d;
+            this.temperature = temperature;
+            this.humidity = humidity;
+        }
+
+        public String toString() {
+            return time.getTime() + 
+            String.format(" temperature: %1$.1f humidity: %2$.2f", temperature, humidity);
+        }
+    }
+
+    private Calendar lastTime = Calendar.getInstance();
+    {
+        lastTime.set(Calendar.MINUTE, 30);
+        lastTime.set(Calendar.SECOND, 00);
+    }
+
+    private float  lastTemp          = 65.0f;
+    private int    tempDirection     = +1;
+    private float  lastHumidity      = 50.0f;
+    private int    humidityDirection = +1;
+    private Random rand              = new Random(47);
+
+    List<DataPoint> data = Collections.synchronizedList(new ArrayList<DataPoint>());
+
+
+    class CollectData implements Runnable {
+
+        public void run() {
+            System.out.println("Collecting data...");
+            synchronized(GreenhouseScheduler.this) {
+                lastTime.set(Calendar.MINUTE, lastTime.get(Calendar.MINUTE) + 30);
+
+                if (rand.nextInt(5) == 4) {
+                    tempDirection = -tempDirection;
+                }
+                lastTemp = lastTemp + tempDirection * (1.0f + rand.nextFloat());
+
+                if (rand.nextInt(5) == 4) {
+                    humidityDirection = - humidityDirection;
+                }
+                lastHumidity = lastHumidity + humidityDirection * rand.nextFloat();
+
+                data.add(new DataPoint((Calendar)lastTime.clone(), lastTemp, lastHumidity));
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        GreenhouseScheduler gh = new GreenhouseScheduler();
+        gh.schedule(gh.new Terminate(), 5000);
+        gh.repeat(gh.new Bell(), 0, 1000);
+        gh.repeat(gh.new ThermostatNight(), 0, 2000);
+        gh.repeat(gh.new LightOn(), 0, 200);
+        gh.repeat(gh.new LightOff(), 0, 400);
+        gh.repeat(gh.new WaterOn(), 0, 600);
+        gh.repeat(gh.new WaterOff(), 0, 800);
+        gh.repeat(gh.new ThermostatDay(), 0, 1400);
+        gh.repeat(gh.new CollectData(), 500, 500);
     }
 }
