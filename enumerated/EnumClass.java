35a1d0ae22968a138a507273f86cb5306b016ef9
java-tester-x <iohonko@gmail.com>
2015-04-25 01:50:54 +0300

    Enumereted: using simple enum example

---
+++ enumerated/EnumClass.java
@@ -0,0 +1,34 @@
+package enumerated;
+
+import java.util.*;
+
+/**
+ * RUN:
+ *         javac enumerated/EnumClass.java && java enumerated.EnumClass
+ * OUTPUT:
+ *        
+ */
+
+
+enum Shrubbery { GROUND, CRAWLING, HANGING }
+
+
+public class EnumClass {
+    
+    public static void main(String[] args) {
+        for (Shrubbery s : Shrubbery.values()) {
+            System.out.println(s + " ordinal: " + s.ordinal());
+            System.out.print(s.compareTo(Shrubbery.CRAWLING) + " ");
+            System.out.print(s.equals(Shrubbery.CRAWLING) + " ");
+            System.out.println(s == Shrubbery.CRAWLING);
+            System.out.println(s.getDeclaringClass());
+            System.out.println(s.name());
+            System.out.println("---------------------------------");
+        }
+
+        for (String s : "HANGING CRAWLING GROUND".split(" ")) {
+            Shrubbery shrub = Enum.valueOf(Shrubbery.class, s);
+            System.out.println(shrub);
+        }
+    }
+}
