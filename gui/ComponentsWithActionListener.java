5d494b5e9a3de9243b2d678d16e2b8436fb13202
java-tester-x <iohonko@gmail.com>
2015-05-28 07:46:03 +0300

    GUI: components with ActionListener interface

--- gui/ComponentsWithActionListener.java
+++ gui/ComponentsWithActionListener.java
@@ -47,6 +47,44 @@ public class ComponentsWithActionListener extends JFrame  {
     protected JTextArea textArea;
     private final static String newline = "\n";
 
+    private ActionListener action = new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            String name = new String("x");
+            if (e.getSource() instanceof JButton ) {
+                name = ((JButton)e.getSource()).getText();
+            }
+            else if (e.getSource() instanceof JCheckBox) {
+                name = ((JCheckBox)e.getSource()).getText();
+            }
+            else if (e.getSource() instanceof JRadioButton) {
+                name = ((JRadioButton)e.getSource()).getText();
+            }
+            else if (e.getSource() instanceof JMenuItem) {
+                name = ((JMenuItem)e.getSource()).getText();
+            }
+            else if (e.getSource() instanceof JCheckBoxMenuItem) {
+                name = ((JMenuItem)e.getSource()).getText();
+            }
+            else if (e.getSource() instanceof JRadioButtonMenuItem) {
+                name = ((JCheckBoxMenuItem)e.getSource()).getText();
+            } 
+            else if (e.getSource() instanceof JToggleButton) {
+                name = ((JToggleButton)e.getSource()).getText();
+            } 
+            else if (e.getSource() instanceof JTextField) {
+                name = ((JTextField)e.getSource()).getText();
+            } 
+            else if (e.getSource() instanceof JFormattedTextField) {
+                name = ((JFormattedTextField)e.getSource()).getText();
+            }
+            else if (e.getSource() instanceof JPasswordField) {
+                name = ((JPasswordField)e.getSource()).getText();
+            }  
+            textArea.append(name + newline);
+            
+        }
+    };
+
     public ComponentsWithActionListener() {
         super();
 
@@ -75,11 +113,27 @@ public class ComponentsWithActionListener extends JFrame  {
         panel.add(textField, c);
         panel.add(formattedTextField, c);
         formattedTextField.setColumns(20);
+
+        panel.add(passwordField, c);
  
         c.fill = GridBagConstraints.BOTH;
         c.weightx = 1.0;
         c.weighty = 1.0;
         panel.add(scrollPane, c);
+
+
+        b.addActionListener(action);
+        cb.addActionListener(action);
+        rb.addActionListener(action);
+        tb.addActionListener(action);
+        textField.addActionListener(action);
+        formattedTextField.addActionListener(action);
+        passwordField.addActionListener(action);
+
+        panel.add(b);
+        panel.add(rb);
+        panel.add(cb);
+        panel.add(tb);
 
         add(panel);
         
